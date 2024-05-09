0a4816564a5c3d5c8bf84a62330bda360437e89f
Yash kandiyal <yashkandiyal22@gmail.com>
2024-05-09 20:02:38 +0530

    task 2 and 3

--- backend/index.js
+++ backend/index.js
@@ -24,10 +24,23 @@ app.post("/one-shot", upload.single("inputFile"), (req, res) => {
     res.send(400).json("provide a file");
   }
   console.log(file);
-
+  const chunkSize = 100 * 1024 * 1024;
+  const fileSize = file.size;
+  const NumberOfChunks = Math.ceil(fileSize / chunkSize);
+  console.log(NumberOfChunks);
   res.json({ message: "File uploaded successfully", file: file });
 });
-app.post("/parallel-upload", (req, res) => {});
+app.post("/parallel-upload", upload.single("inputFile"), (req, res) => {
+  // so the approach is simple whatever the size of the file is we will divide it by the chunk size we want
+  const file = req.file;
+  if (!file) {
+    res.send(400).json("provide a file");
+  }
+  const chunkSize = 100 * 1024 * 1024;
+  const fileSize = file.size;
+  const NumberOfChunks = Math.ceil(fileSize / chunkSize);
+});
+app.post("/aws-upload", upload.single("inputFile"), (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`);
