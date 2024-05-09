7fc105854ccd1adc05caaa01970b8fbed87e89cf
Yash kandiyal <yashkandiyal22@gmail.com>
2024-05-09 20:18:30 +0530

    implemented s3 bucket

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const fs = require("fs");
 const multer = require("multer");
 const status = require("express-status-monitor");
+const { PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 const storage = multer.diskStorage({
   destination: function (req, res, cb) {
     cb(null, "uploads/");
@@ -13,7 +13,21 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage: storage });
+//There was some problem in payment for aws account my card was getting rejected so i have used dummy data.
+const cloudS3Storage = multer.memoryStorage();
+const uploadCloud = multer({ storage: cloudS3Storage });
 
+const bucketName = "name";
+const bucketRegion = "region";
+const accessKey = "key";
+const secretAccessKey = "secretKey";
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey: secretAccessKey,
+  },
+  region: bucketRegion,
+});
 app.use(cors());
 app.use(status());
 const PORT = 3000;
@@ -40,8 +54,25 @@ app.post("/parallel-upload", upload.single("inputFile"), (req, res) => {
   const fileSize = file.size;
   const NumberOfChunks = Math.ceil(fileSize / chunkSize);
 });
-app.post("/aws-upload", upload.single("inputFile"), (req, res) => {});
-app.post("/parallel-upload", upload.single("inputFile"), (req, res) => {});
+app.post("/aws-upload", uploadCloud.single("inputFile"), async (req, res) => {
+  const file = req.file;
+  if (!file) {
+    res.send(400).json("provide a file");
+  }
+
+  try {
+    const params = {
+      Bucket: bucketName,
+      Key: file.originalname,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    };
+    const command = new PutObjectCommand(params);
+    await s3.send(command);
+  } catch (error) {
+    console.log("error", error);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`);
